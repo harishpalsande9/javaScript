485201dab1223f55bdd64bbbc8ef0382d949860d
harishpalsande9 <harishpalsande9@gmail.com>
2023-02-13 17:16:50 +0530

    generator

--- test2.js
+++ test2.js
@@ -49,16 +49,77 @@
 
 // Closures
 
-let a = 10;
-function fun1() {
-  let b = 20;
-  function fun2() {
-    console.log(a);
-    console.log(b);
-  }
-  fun2();
-}
+// let a = 10;
+// function fun1() {
+//   let b = 20;
+//   function fun2() {
+//     console.log(a);
+//     console.log(b);
+//   }
+//   fun2();
+// }
 
+// fun1();
 
+// NaN => Not a number
+// null =>  value null , null data type object
 
-fun1();
+// console.log(typeof null);
+
+// function
+// function fun1(name, callback) {
+//   console.log("Hi" + " " + name);
+//   callback();
+// }
+
+// // callback function
+// function fun2() {
+//   console.log("I am callback function");
+// }
+
+// // passing function as an argument
+// fun1("Peter", fun2);
+
+// const fun1 = (name, callBack) => {
+//   console.log("Hi" + " " + name);
+//   callBack();
+// };
+
+// const fun2 = () => {
+//   console.log("I am callback function");
+// };
+
+// fun1("harish", fun2);
+
+// function memoizedAddTo256() {
+//   var cache = {};
+
+//   return function (num) {
+//     if (num in cache) {
+//       console.log("cached value");
+//       return cache[num];
+//     } else {
+//       cache[num] = num + 256;
+//       return cache[num];
+//     }
+//   };
+// }
+// var memoizedFunc = memoizedAddTo256();
+
+// memoizedFunc(20); // Normal return
+// memoizedFunc(20); // Cached return
+
+// const fun2 = (...input) => {
+//   console.log(input);
+// };
+
+// fun2(234, 234, 234, 234, 2);
+
+// 36. In JavaScript, how many different methods can you make an object?
+
+// let Obj = {
+//   name: "Object1",
+// };
+
+// let obj2 = new Object();
+//39. What are generator functions?
