8cb5a2c515885e028bbf16ea15d9e0b761c952a5
harishpalsande9 <harishpalsande9@gmail.com>
2023-02-19 23:46:47 +0530

    Event Loop

--- test3.js
+++ test3.js
@@ -22,10 +22,36 @@
 
 //event bubbling in javascript
 
-document.getElementById("FirstDiv").addEventListener("click", () => {
-  alert("FirstDiv clicked");
+// document.getElementById("FirstDiv").addEventListener("click", () => {
+//   alert("FirstDiv clicked");
+// });
+
+// document.getElementById("secondDiv").addEventListener("click", () => {
+//   alert("second clicked");
+// });
+
+//Event Loop
+
+console.log("Start");
+
+setTimeout(() => {
+  console.log("midlle");
+}, 5000);
+
+let first = new Promise((resolve, reject) => {
+  let a = 10;
+
+  if (a === 10) {
+    resolve("Scucess");
+  } else {
+    reject("Failed");
+  }
 });
 
-document.getElementById("secondDiv").addEventListener("click", () => {
-  alert("second clicked");
-});
+first.then((ele) => console.log(ele)).catch((err) => console.log(err));
+
+Promise.all([first, second])
+  .then((result) => console.log(result))
+  .catch((err) => console.log(err));
+
+console.log("End");
